cdc82d908c70daddc45d689eebe9370420c22446
Selma Hadad <naderhadadsouza@gmail.com>
2023-11-01 20:03:58 -0300

    criar recursos

--- terraform/azure/provider.tf
+++ terraform/azure/provider.tf
@@ -14,8 +14,8 @@ terraform {
     # }
   }
   backend "azurerm" {
-    resource_group_name  = "rg-staticsite-vm-tf"
-    storage_account_name = "staticsitevmkbtf001"
+    resource_group_name  = "rg-nader-cp3"
+    storage_account_name = "nadercp3"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
--- terraform/azure/vars.tf
+++ terraform/azure/vars.tf
@@ -1,6 +1,6 @@
 variable "rg_name" {
     type    = string
-    default = "rg-staticsite-vm"
+    default = "rg-nader-cp3-v1"
 }
 
 variable "location" {
