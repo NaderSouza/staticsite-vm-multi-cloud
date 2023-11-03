0997d3fbac878c8359295116cc485789dd78897b
Nader Hadad Souza <naderhadadsouza@gmail.com>
2023-11-02 23:32:17 -0300

    bonus

--- terraform/azure/modules/compute/vm.tf
+++ terraform/azure/modules/compute/vm.tf
@@ -85,30 +85,31 @@ resource "azurerm_virtual_machine" "vm" {
   vm_size                          = "Standard_DS1_v2"
   delete_os_disk_on_termination    = true
   delete_data_disks_on_termination = true
+  
   storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
+  
   storage_os_disk {
     name              = "staticsite-vm-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
-
+  
   os_profile {
     computer_name  = "staticsite-vm"
     admin_username = "vmuser"
-
-    custom_data = base64encode(data.template_file.cloud_init.rendered)
+    custom_data    = base64encode(data.template_file.cloud_init.rendered)
   }
 
   os_profile_linux_config {
     disable_password_authentication = true
     ssh_keys {
-      key_data = azurerm_ssh_public_key.example.public_key
+      key_data = azurerm_ssh_public_key.nadin-ssh.public_key
       path     = "/home/vmuser/.ssh/authorized_keys"
     }
   }
